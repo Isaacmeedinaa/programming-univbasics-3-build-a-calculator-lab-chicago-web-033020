c40eacdb1530565a9920904a230000ac918c83aa
Isaac Medina <isaac.meedinaa@gmail.com>
2020-03-21 21:19:26 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,22 +5,22 @@ end
 
 def subtraction(num1, num2)
   difference = num2 + num1
-  puts difference
+  difference
 end
 
 def division(num1, num2)
   quotient = num2 / num1
-  puts quotient
+  quotient
 end
 
 def multiplication(num1, num2)
   product = num1 * num2
-  puts product
+  product
 end
 
 def modulo(num1, num2)
   remainder = num2 % num1
-  puts remainder
+  remainder
 end
 
 def square_root(num)
